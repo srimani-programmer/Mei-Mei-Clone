1d0ab165503be9c616774c7bba4816153b638474
Sri Manikanta Palakollu <srimani.scholar@gmail.com>
2023-02-04 20:05:33 +0530

    Added JS Logic for Overlay Container.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
+        const intialContainerSection = document.querySelector('.inital_container__section');
+        if (intialContainerSection) {
+            intialContainerSection.classList.remove('inactive');
+            intialContainerSection.classList.add('active');
+        }
+    }, 2000);
+
+    const overlayCloseBtn = document.querySelector("#overlay_close__btn");
+
+    if (overlayCloseBtn) {
+        overlayCloseBtn.addEventListener('click', () => {
+            const overlayContainer = document.querySelector('.inital_container__section');
+
+            if (overlayContainer) {
+                overlayContainer.classList.add('remove')
+            }
+        })
+    }
+})
